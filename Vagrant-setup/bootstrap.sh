69a41a4573d7fc41af159632e79ae82f14df47cc
jvf@home <web@jens-fischer.eu>
2014-11-29 17:38:33 +0100

    vagrant: add imposm to bootstrap

--- Vagrant-setup/bootstrap.sh
+++ Vagrant-setup/bootstrap.sh
@@ -89,6 +89,11 @@ CREATE USER $APP_DB_USER WITH PASSWORD '$APP_DB_PASS';
 CREATE DATABASE $APP_DB_NAME WITH OWNER $APP_DB_USER;
 EOF
 
+wget --quiet -O imposm3.tar.gz  http://imposm.org/static/rel/imposm3-0.1dev-20140811-3f3c12e-linux-x86-64.tar.gz
+mkdir imposm3
+tar -zxf imposm3.tar.gz -C imposm3 --strip-components=1
+rm imposm3.tar.gz
+
 # Tag the provision time:
 date > "$PROVISIONED_ON"
 
