50a7e4e29cb0a16214a8bf4ff6ddba1584797fd5
jo <kleingeist@googlemail.com>
2014-12-10 00:22:13 +0100

    Add range parameter to gfs download

--- importer/gfs_download.py
+++ importer/gfs_download.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from urllib.request import urlretrieve
 import argparse
 import os
@@ -41,38 +41,49 @@ def prepare_outputdir(output, modeldate, keep=False):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("modeldate", type=str, default="latest")
+    parser.add_argument("--download-range", type=str, default=None)
     parser.add_argument("output", type=str)
     parser.add_argument("--hours_start", type=int, default=0)
-    parser.add_argument("--hours_stop", type=int, default=128)
+    parser.add_argument("--hours_stop", type=int, default=129)
     parser.add_argument("--hours_step", type=int, default=3)
     args = parser.parse_args()
 
     if args.modeldate == "latest":
         modeldate = datetime.now().replace(minute=0, second=0, microsecond=0)
-        modeldate = modeldate.replace(hour=modeldate.hour - 6)
+        modeldate = modeldate - timedelta(hours=6)
     else:
         modeldate = datetime.strptime(args.modeldate, "%Y%m%d%H")
     modeldate = modeldate.replace(hour=(modeldate.hour // 6) * 6)
 
     hours = range(args.hours_start, args.hours_stop + 1, args.hours_step)
-    outdir = prepare_outputdir(args.output, modeldate)
+    if args.download_range:
+        enddate = datetime.strptime(args.download_range, "%Y%m%d%H")
+        enddate = enddate.replace(hour=(enddate.hour // 6) * 6)
+    else:
+        enddate = modeldate
 
-    for hour in hours:
-        url_ = URL.format(model_date=modeldate.strftime("%Y%m%d"),
-                          model_hour=modeldate.hour,
-                          forecast_hour=hour)
-        file = "gfs.t{model_hour}z.mastergrb2f{forecast_hour:02}".format(
-            model_date=modeldate.strftime("%Y%m%d"),
-            model_hour=modeldate.hour,
-            forecast_hour=hour
-        )
+    while modeldate <= enddate:
 
-        out = path.join(outdir, file)
-        print("Going to download '{url}' to '{out}'".format(url=url_, out=out))
-        urlretrieve(url_, out)
+        outdir = prepare_outputdir(args.output, modeldate)
 
-        # Be nice and sleep a milli second
-        time.sleep(0.1)
+        for hour in hours:
+            url_ = URL.format(model_date=modeldate.strftime("%Y%m%d"),
+                              model_hour=modeldate.hour,
+                              forecast_hour=hour)
+            file = "gfs.t{model_hour}z.mastergrb2f{forecast_hour:02}".format(
+                model_date=modeldate.strftime("%Y%m%d"),
+                model_hour=modeldate.hour,
+                forecast_hour=hour
+            )
+
+            out = path.join(outdir, file)
+            print("Going to download '{url}' to '{out}'".format(url=url_, out=out))
+            urlretrieve(url_, out)
+
+            # Be nice and sleep some milli seconds
+            time.sleep(0.01)
+
+        modeldate = modeldate + timedelta(hours=6)
 
 
 if __name__ == '__main__':
