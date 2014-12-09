4cf0ccfab271fad90caaa013215d47c7a1b6bc89
jensvfischer <jensvfischer@gmail.com>
2014-12-09 19:21:53 +0100

    cleanup manager

--- manage.py
+++ manage.py
@@ -9,11 +9,6 @@ import json
 
 manager = Manager(app)
 
-@manager.option('-b, --bool', dest='test', help="bool", default=False, required=False, metavar="foobar")
-def test(test):
-    "test command"
-    print(test)
-
 
 @manager.option('--imposm', action='store_true', dest='imposm', default=False,
                 help="import data with imposm3 from pfb file")
@@ -29,7 +24,7 @@ def import_osm(imposm, load, drop_tables):
 
 @manager.option('-t', '--tables', dest='tables', help="osm, map, dwd, gfs", required=True)
 def drop_tables(tables):
-    "drop the tables for osm (osm_admin, osm_places), map (country, state, district, cities) or dwd (station, observation)"
+    """"drop the tables for osm (osm_admin, osm_places), map (country, state, district, cities) or dwd (station, observation)"""
 
     if tables == 'osm':
         Osm_Admin.__table__.drop(db.engine, checkfirst=True)
@@ -50,11 +45,10 @@ def drop_tables(tables):
 DEFAULT_FILE_NAME = 'data/weather.json'
 @manager.option('--to_json', action='store_true', dest='to_json', default=False,
                 help="import data to json")
-@manager.option('--load_from_json', action='store_true', dest='load_from_json', default=False,
+@manager.option('--_from_json', action='store_true', dest='load_from_json', default=False,
                 help="import from json file")
-# @manager.option('--drop_tables', action='store_true', dest='drop_tables', default=False,
-#                 help="drop the osm_admin and osm_places tables from the imposm import")
 def import_dwd(to_json, load_from_json, file=DEFAULT_FILE_NAME):
+    """import dwd weather data, either directly to the database or to/from a json file"""
     if to_json:
         if os.path.isfile(file):
             override = input('%s already exists. Override? [y/N]' % file)
@@ -63,7 +57,7 @@ def import_dwd(to_json, load_from_json, file=DEFAULT_FILE_NAME):
             else:
                 print("aborting...")
                 return
-        import_dwd_json(limit=3)
+        import_dwd_json()
     elif load_from_json:
         data = json.load(open(file, 'r'))
         import_dwd_from_json(data)
