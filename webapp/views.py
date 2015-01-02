f09906f8b7501352e508ff56db0e731f71a376d7
jvf@home <web@jens-fischer.eu>
2015-01-02 18:30:37 +0100

    add info/district.json view

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,5 @@
 from webapp import app, db
-from models.map import Country, District, Station, Observation
+from models.map import Country, State, District, Station, Observation
 from flask import render_template, request, abort, json
 from geoalchemy2 import functions as func
 from datetime import datetime
@@ -30,6 +30,8 @@ def dummy(path):
 def info(request_type):
     """ example queries:
         http://127.0.0.1:5000/info/station.json?lat=8.237&lon=52.9335&forecast=False&datetime=2014120100
+        http://127.0.0.1:5000/info/district.json?lat=10.237&lon=52.9335&forecast=False&datetime=2014120100
+        http://127.0.0.1:5000/info/district.json?lat=9.237&lon=52.9335&forecast=False&datetime=2014120100 (example for no stations in district)
     """
     lat = float(request.args.get("lat"))
     lon = float(request.args.get("lon"))
@@ -40,14 +42,33 @@ def info(request_type):
     if request_type == 'station' and forecast not in ['true', 'True']:
         station = db.session.query(Station.dwd_id, Station.name, Station.altitude,
                                    Station.geometry.ST_AsGeoJSON().label('geometry'), Observation.date,
-                                   Observation.rainfall, Observation.temperature).filter(
-            Observation.station_id == Station.id, Observation.date == request_datetime,
-            func.ST_Intersects(Station.region, 'SRID=4326;POINT(%s %s)' % (lat, lon))).first()
+                                   Observation.rainfall, Observation.temperature) \
+            .filter(Observation.station_id == Station.id, Observation.date == request_datetime,
+                    func.ST_Intersects(Station.region, 'SRID=4326;POINT(%s %s)' % (lat, lon))) \
+            .first()
 
         feature = to_feature(station._asdict())
         return json.jsonify(feature)
+
+    elif request_type == "district":
+        district = District.query.filter(District.geometry.ST_Intersects('SRID=4326;POINT(%s %s)' % (lat, lon))).first()
+        geometry = db.session.scalar(func.ST_AsGeoJSON(district.geometry))
+
+        response_builder = {"name": district.name, "admin_entity": District.__tablename__,
+                            "admin_level": district.admin_level,
+                            "geometry": geometry, "observations": {}}
+
+        '''
+        intersect between region (voronoi cell) of a station and the district geometry, because there are districts
+        which don't have any stations in them, e.g. Landkreis Verden
+        '''
+        stations = Station.query.filter(Station.region.ST_Intersects(district.geometry)).all()
+        calc_means(request_datetime, response_builder, stations, district)
+
+        response = to_feature(response_builder)
+        return json.jsonify(response)
     else:
-        return json.jsonify(response="not yet implemented")
+        abort(404)
 
 
 @app.route('/stations.json')
@@ -74,3 +95,30 @@ def to_feature(fields):
 
     feature["properties"] = properties
     return feature
+
+
+def calc_means(request_datetime, response_builder, stations, admin_entity):
+    """
+    Calculates the weighted arithmetic mean.
+    The mean is weighted by the area a region of a station contributes to the admin_entity.
+    """
+    mean_temp, mean_rain, weight_temp, weight_rain = 0, 0, 0, 0
+    for station in stations:
+        observation = Observation.query.filter(Observation.station_id == station.id,
+                                               Observation.date == request_datetime).first()
+        contributing_area = db.session.scalar(func.ST_Area(func.ST_Intersection(station.region, admin_entity.geometry)))
+        # ignore -999 values
+        if observation.temperature != -999:
+            mean_temp += (observation.temperature * contributing_area)
+            weight_temp += contributing_area
+        if observation.rainfall != -999:
+            mean_rain += (observation.rainfall * contributing_area)
+            weight_rain += contributing_area
+
+        # uncomment for debugging
+        # response_builder["observations"][station.name] = {"id": observation.id, "date": observation.date,
+        #                                                   "temperature": observation.temperature,
+        #                                                   "rainfall": observation.rainfall,
+        #                                                   "weight": contributing_area}
+    response_builder["mean temperature"] = mean_temp / weight_temp
+    response_builder["mean rainfall"] = mean_rain / weight_rain
