c80ee26ea619e8b52b9ae80363d5ef0f4471fad6
jvf@home <web@jens-fischer.eu>
2015-01-10 14:31:39 +0100

    disable jsonify pretty printing
    increases performance, e.g. factor 5 for forecast/state.json

--- webapp/config.py
+++ webapp/config.py
@@ -6,5 +6,7 @@ DB_PW = 'dbpass'
 DB_HOST = 'localhost'
 DB_PORT = 15432
 DB_NAME = 'spatial'  # assumes an existing db 'spatial' with postgis extension
+JSONIFY_PRETTYPRINT_REGULAR = False
+JSON_SORT_KEYS = False
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PW + '@' + DB_HOST + ':' + str(DB_PORT) + '/' + DB_NAME
